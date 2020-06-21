3b15c8d6933cfd804450e8b1289c1e6e0197654a
gustavolopesv3 <gustavo.lopesv3@gmail.com>
2020-06-21 13:47:52 -0500

    real

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class HomePage extends StatelessWidget {
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tr.value.toString(),
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
